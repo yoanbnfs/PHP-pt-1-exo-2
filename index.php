06cb1cf25395961138c5d8e42cd66130c261332d
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-12-13 09:04:32 +0100

    ajout de l'html

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-<?php
-  $nom = "Bonnefis";
-  $prenom = "Yoan";
-  $age = 26;
-  echo "nom : " . $nom  . " prenom : " . $prenom . " age : " . $age; 
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exo2</title>
+  </head>
+  <body>
+    <?php
+      $nom = 'Bonnefis';
+      $prenom = 'Yoan';
+      $age = 26;
+      echo 'nom : ' . $nom  . ' prenom : ' . $prenom . ' age : ' . $age; 
+    ?>
+  </body>
+</html>
